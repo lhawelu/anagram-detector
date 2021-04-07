f5f8cb1f53028e229aff7ea1014f50564d01d6a5
Larry Hawelu <lhawelu@gmail.com>
2021-04-07 15:38:12 -0700

    commit tree 55a82899b523e2fe9d4a71ea19d39f8d7257d580

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match array
+    array.select {|element| element.chars.sort(&:casecmp).join == @word.chars.sort(&:casecmp).join}
+  end
+
+end
